d97cf84d946f958fa1fa311af52ffe2752905b1a
Rodolfo Costa <rodolfombc@gmail.com>
2016-02-11 03:21:23 -0200

    Player adicionado a cena de jogo, mas sem animações ou movimentos

---
+++ gyro_twist/Assets/Scripts/play_button.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play_button : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        Debug.Log("Going to the game");
+        Application.LoadLevel("game");
+    }
+}
