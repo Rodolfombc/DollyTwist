55d0af851d56cfe0f296327a9c5b3053d53e27b4
Rodolfo Costa <rodolfombc@gmail.com>
2016-02-12 15:44:19 -0200

    Colisão do jogador com o chão adicionada

--- gyro_twist/Assets/Scripts/gravityHandler.cs
+++ gyro_twist/Assets/Scripts/gravityHandler.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEditor;
 
 public class gravityHandler : MonoBehaviour {
 
--- gyro_twist/Assets/Scripts/play_button.cs
+++ gyro_twist/Assets/Scripts/play_button.cs
@@ -5,7 +5,8 @@ public class play_button : MonoBehaviour {
 
     void OnMouseDown()
     {
-        Debug.Log("Going to the game");
+        //Debug.Log("Going to the game");
+        Physics2D.gravity = new Vector2(0.0f, -9.8f);
         Application.LoadLevel("game");
     }
 }
