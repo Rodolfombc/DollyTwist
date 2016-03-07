e3c9a527249f99235a514a80b8e3b103023e7d4c
Rodolfo Costa <rodolfombc@gmail.com>
2016-03-07 00:44:32 -0300

    Commit final para entregar

---
+++ src/highscore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class highscore : MonoBehaviour {
+
+    public Text score;
+
+    void OnMouseDown()
+    {
+        //Debug.Log("Ver scores");
+        Application.LoadLevel("scores");
+    }
+
+    
+    void Start () {
+        score.text = PlayerPrefs.GetInt("score").ToString() + " metros";
+    }
+}
